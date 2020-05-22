181415dbf934dd9fc7c7f824a39b62ec58c14ba0
John D. Boy <jboy@bius.moe>
2020-05-22 02:44:39 +0200

    Update copyright line for docs.

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Textnets'
-copyright = u"2017, John D. Boy"
+copyright = u"2017-2020, John D. Boy"
 
 # The version info for the project you're documenting, acts as replacement
 # for |version| and |release|, also used in various other places throughout
