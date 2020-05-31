acbf25b5137518b7105d2e68344dc30280ccd05f
John D. Boy <jboy@bius.moe>
2020-05-31 02:33:54 +0200

    add flake8 config, default.nix for nix-shell

---
+++ default.nix
@@ -0,0 +1,21 @@
+with import <nixpkgs> {};
+
+  stdenv.mkDerivation rec {
+  name = "env";
+  env = buildEnv { name = name; paths = buildInputs; };
+  venvDir = "./VENV";
+  buildInputs = [
+    python38Full
+    python38Packages.venvShellHook
+    python38Packages.virtualenv
+
+    python38Packages.click
+    python38Packages.pandas
+    python38Packages.pycairo
+    python38Packages.spacy
+    python38Packages.toolz
+  ];
+  postShellHook = ''
+    pip install -r requirements_dev.txt
+  '';
+}
