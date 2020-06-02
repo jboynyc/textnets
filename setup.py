7b3729fbcecf5df8f057c4e29d0964b38c43db4a
John D. Boy <jboy@bius.moe>
2020-06-02 15:33:49 +0200

    Bump version: 0.3.3 → 0.3.4

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     author_email="jboy@bius.moe",
     python_requires=">=3.8",
     name="textnets",
-    version="0.3.3",
+    version="0.3.4",
     description="Automated text analysis with networks",
     long_description=readme,
     url="https://textnets.readthedocs.io",
