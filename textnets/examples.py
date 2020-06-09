2ff4aeea728e19e227ea9f9eea2d50576cf50314
John D. Boy <jboy@bius.moe>
2020-06-10 00:26:18 +0200

    example data is now a Series, not a DataFrame

--- textnets/examples.py
+++ textnets/examples.py
@@ -6,27 +6,23 @@ import pandas as pd
 
 
 #: Example dataset with newspaper headlines about the Apollo 11 landing.
-moon_landing = pd.DataFrame(
-    {
-        "paper": [
-            "The Guardian",
-            "New York Times",
-            "Boston Globe",
-            "Houston Chronicle",
-            "Washington Post",
-            "Chicago Tribune",
-            "Los Angeles Times",
-        ],
-        "headline": [
-            "3:56 am: Man Steps On to the Moon",
-            "Men Walk on Moon -- Astronauts Land on Plain, "
-            "Collect Rocks, Plant Flag",
-            "Man Walks on Moon",
-            'Armstrong and Aldrich "Take One Small Step for Man" on the Moon',
-            "The Eagle Has Landed -- Two Men Walk on the Moon",
-            "Giant Leap for Mankind -- Armstrong Takes 1st Step on Moon",
-            "Walk on Moon -- That's One Small Step for Man, "
-            "One Giant Leap for Mankind",
-        ],
-    }
-).set_index("paper")
+moon_landing = pd.Series(
+    [
+        "3:56 am: Man Steps On to the Moon",
+        "Men Walk on Moon -- Astronauts Land on Plain, " "Collect Rocks, Plant Flag",
+        "Man Walks on Moon",
+        'Armstrong and Aldrich "Take One Small Step for Man" on the Moon',
+        "The Eagle Has Landed -- Two Men Walk on the Moon",
+        "Giant Leap for Mankind -- Armstrong Takes 1st Step on Moon",
+        "Walk on Moon -- That's One Small Step for Man, " "One Giant Leap for Mankind",
+    ],
+    index=[
+        "The Guardian",
+        "New York Times",
+        "Boston Globe",
+        "Houston Chronicle",
+        "Washington Post",
+        "Chicago Tribune",
+        "Los Angeles Times",
+    ],
+)
