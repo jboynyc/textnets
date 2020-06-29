00ca19384e88b5ac3a4614f9ab9cdb4138073267
John D. Boy <jboy@bius.moe>
2020-06-29 13:51:37 +0200

    add conftest

---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Configure tests for `textnets` package."""
+
+import pytest
+from textnets import Corpus, Textnet, examples
+
+
+@pytest.fixture(scope="session")
+def testdata():
+    return examples.moon_landing
+
+
+@pytest.fixture(scope="session")
+def corpus(testdata):
+    return Corpus(testdata)
