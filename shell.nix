b0a5f771c70b64eff06fb73d9d7d4ca6f5ec6e65
John D. Boy <jboy@bius.moe>
2021-06-25 15:47:35 +0200

    use poetry to handle dependencies

--- shell.nix
+++ shell.nix
@@ -2,27 +2,21 @@
 
 with pkgs;
 
-stdenv.mkDerivation rec {
+mkShell rec {
   name = "env";
   env = buildEnv { name = name; paths = buildInputs; };
   venvDir = "./.VENV";
   buildInputs = [
-    python38Full
-    python38Packages.venvShellHook
-    python38Packages.virtualenv
-
-    python38Packages.pandas
-    python38Packages.cairocffi
-    python38Packages.spacy
-
-    python38Packages.toolz
-
-    python38Packages.jupyter
-    python38Packages.matplotlib
+    cairo
+    poetry
+    python3
+    python3.pkgs.venvShellHook
   ];
   postShellHook = ''
+    export LD_LIBRARY_PATH=${stdenv.cc.cc.lib}/lib/:${cairo}/lib/:$LD_LIBRARY_PATH
     export PS1="\$(__git_ps1) $PS1"
-    pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.0.0/en_core_web_sm-3.0.0.tar.gz#egg=en_core_web_sm-3.0.0
-    pip install -e ".[dev,doc]"
+    poetry install
+    poetry install -E doc
+    poetry install -E test
   '';
 }
