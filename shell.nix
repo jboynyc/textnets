4c04c190f88e6b0e44d1d285d0f3c2879e8ac942
John Boy <2187261+jboynyc@users.noreply.github.com>
2022-05-06 11:30:39 +0200

    set zlib path in nix-shell

--- shell.nix
+++ shell.nix
@@ -6,12 +6,13 @@ mkShell rec {
   venvDir = "./.VENV";
   buildInputs = [
     cairo
+    zlib
     poetry
     python39.pkgs.venvShellHook
   ];
   postShellHook = ''
     export PS1="\$(__git_ps1) $PS1"
-    export LD_LIBRARY_PATH=${stdenv.cc.cc.lib}/lib/:${cairo}/lib/:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=${stdenv.cc.cc.lib}/lib/:${cairo}/lib/:${zlib}/lib:$LD_LIBRARY_PATH
     poetry install
     poetry install -E doc
     spacy validate | grep en_core_web_sm || poetry run pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.3.0/en_core_web_sm-3.3.0.tar.gz
