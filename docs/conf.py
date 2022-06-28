61d982f340e323a4d6e7550e75730b5504af92b5
John Boy <2187261+jboynyc@users.noreply.github.com>
2022-06-28 13:08:33 +0200

    update docs config for sphinx 5

--- docs/conf.py
+++ docs/conf.py
@@ -26,10 +26,6 @@ import textnets
 
 # -- General configuration ---------------------------------------------
 
-# If your documentation needs a minimal Sphinx version, state it here.
-#
-# needs_sphinx = '1.0'
-
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
@@ -82,7 +78,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "textnets"
-copyright = "2021"
+copyright = "2022"
 author = "John D. Boy"
 
 # The version info for the project you're documenting, acts as replacement
@@ -99,7 +95,7 @@ release = version
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
