dd931b4aeedc6d8483cde2b40fda84d6bca98be8
John Boy <2187261+jboynyc@users.noreply.github.com>
2022-07-12 14:08:29 +0200

    add graphviz to dev environment to allow testing FCA features

--- shell.nix
+++ shell.nix
@@ -6,14 +6,15 @@ mkShell rec {
   venvDir = "./.VENV";
   buildInputs = [
     cairo
-    zlib
+    graphviz
     poetry
+    zlib
     python310.pkgs.venvShellHook
   ];
   postShellHook = ''
-    export PS1="\$(__git_ps1) $PS1"
     export LD_LIBRARY_PATH=${stdenv.cc.cc.lib}/lib/:${cairo}/lib/:${zlib}/lib:$LD_LIBRARY_PATH
     poetry install
+    poetry install -E fca
     poetry install -E doc
     spacy validate | grep en_core_web_sm || poetry run pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.4.0/en_core_web_sm-3.4.0.tar.gz
   '';
