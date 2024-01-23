51295e605402775b54e2671162050d455bdf9078
John Boy <2187261+jboynyc@users.noreply.github.com>
2024-01-23 21:17:41 +0100

    using nixpkgs 23.11 in development

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/23.05.tar.gz") {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/23.11.tar.gz") {} }:
 
 with pkgs;
 
