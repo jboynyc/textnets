24ab2f982ba05d544e44aca2736a36ac212bc42e
John D. Boy <2187261+jboynyc@users.noreply.github.com>
2025-03-27 22:45:08 +0100

    try to satisfy setuptools

--- pdm_build.py
+++ pdm_build.py
@@ -1,14 +1,6 @@
-from setuptools import Extension
-
 from Cython.Build import cythonize
 
 
 def pdm_build_update_setup_kwargs(_, setup_kwargs):
-    cy_ext = cythonize(
-        Extension(
-            name="textnets._ext",
-            sources=["textnets/_ext.pyx"],
-        ),
-        compiler_directives={"language_level": 3},
-    )
-    setup_kwargs.update({"ext_modules": cy_ext})
+    cy_ext = cythonize(["textnets/_ext.pyx"], compiler_directives={"language_level": 3})
+    setup_kwargs.update({"packages": ["textnets"], "ext_modules": cy_ext})
