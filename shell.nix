90e5ca720ead6151fd6c8d50482b76c2a4dbf029
John D. Boy <2187261+jboynyc@users.noreply.github.com>
2025-10-13 17:34:22 +0200

    tooling config

--- shell.nix
+++ shell.nix
@@ -5,13 +5,12 @@ with pkgs;
 mkShell rec {
   packages = [
     autoPatchelfHook
-    pdm
     python312Packages.venvShellHook
   ];
   venvDir = "./.venv";
   LD_LIBRARY_PATH="${stdenv.cc.cc.lib}/lib/:$LD_LIBRARY_PATH";  # needed for numpy
   postShellHook = ''
+    ${pkgs.pdm}/bin/pdm install -qd
     autoPatchelf ${venvDir}/bin > /dev/null  # needed for ruff
-    pdm install -qd
   '';
 }
