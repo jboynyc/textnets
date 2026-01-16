a3a890c9de53d856e5eff57a5cf1e1eff887998d
John D. Boy <2187261+jboynyc@users.noreply.github.com>
2026-01-16 09:32:37 +0100

    update shell.nix for 25.11

--- shell.nix
+++ shell.nix
@@ -1,16 +1,17 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/25.05.tar.gz") {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/25.11.tar.gz") {} }:
 
 with pkgs;
 
 mkShell rec {
   packages = [
     autoPatchelfHook
+    pdm
     python312Packages.venvShellHook
   ];
   venvDir = "./.venv";
   LD_LIBRARY_PATH="${stdenv.cc.cc.lib}/lib/:$LD_LIBRARY_PATH";  # needed for numpy
   postShellHook = ''
-    ${pkgs.pdm}/bin/pdm install -qd
+    pdm install -qd
     autoPatchelf ${venvDir}/bin > /dev/null  # needed for ruff
   '';
 }
